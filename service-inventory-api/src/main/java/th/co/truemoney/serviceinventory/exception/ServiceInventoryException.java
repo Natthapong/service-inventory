a101eb63e2bbd60800f2241f9b8f9b6f575fdaef
Tavee Khunbida <taveek@gmail.com>
2013-03-13 16:11:57 +0700

    improve service inventory exception

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/exception/ServiceInventoryException.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/exception/ServiceInventoryException.java
@@ -4,7 +4,28 @@ public abstract class ServiceInventoryException extends RuntimeException {
 	
 	private static final long serialVersionUID = 3961433822995791383L;
 	
-	public abstract String getCode();
-	public abstract String getNamespace();
-	public abstract String getDescription();
+	private String code;
+	private String description;
+	private String namespace;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getNamespace() {
+		return namespace;
+	}
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
+	
+	
 }
