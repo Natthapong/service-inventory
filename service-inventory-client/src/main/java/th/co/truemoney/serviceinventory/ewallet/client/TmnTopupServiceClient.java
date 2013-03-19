164d52115136e81cb7e8a32832daafeb003ffcc0
Tavee Khunbida <taveek@gmail.com>
2013-03-19 15:39:23 +0700

    implement createTopUpQuoteFromDirectDebit to TmnTopUpServiceClient

--- service-inventory-client/src/main/java/th/co/truemoney/serviceinventory/ewallet/client/TmnTopupServiceClient.java
+++ service-inventory-client/src/main/java/th/co/truemoney/serviceinventory/ewallet/client/TmnTopupServiceClient.java
@@ -1,13 +1,26 @@
 package th.co.truemoney.serviceinventory.ewallet.client;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import th.co.truemoney.serviceinventory.ewallet.TopUpService;
 import th.co.truemoney.serviceinventory.ewallet.client.config.EnvironmentConfig;
+import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
+import th.co.truemoney.serviceinventory.ewallet.domain.QuoteRequest;
+import th.co.truemoney.serviceinventory.ewallet.domain.TopUpOrder;
+import th.co.truemoney.serviceinventory.ewallet.domain.TopUpQuote;
+import th.co.truemoney.serviceinventory.ewallet.domain.TopUpStatus;
 import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
-public class TmnTopupServiceClient {
+@Service
+public class TmnTopupServiceClient implements TopUpService {
 	
 	@Autowired
 	RestTemplate restTemplate;
@@ -18,12 +31,53 @@ public class TmnTopupServiceClient {
 	@Autowired
 	private HttpHeaders headers;
 
-	public void verify(Integer channelID, String accesstokenID, String checksum) throws ServiceInventoryException {
+	@Override
+	public TopUpQuote createTopUpQuoteFromDirectDebit(String sourceOfFundID, 
+			QuoteRequest quoteRequest , String accessTokenID) throws ServiceInventoryException {
 		
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("sourceOfFundID", sourceOfFundID);
+		params.put("accessTokenID", accessTokenID);
+		
+		String url = MessageFormat.format(environmentConfig.getCreateTopUpQuoteFromDirectDebitUrl(), params);
+		
+		ResponseEntity<TopUpQuote> responseEntity = restTemplate.postForEntity(url, quoteRequest, TopUpQuote.class);
+		return responseEntity.getBody();
 	}
 	
-	public void order(Integer channelID, String accesstokenID, String checksum) throws ServiceInventoryException {
+	@Override
+	public TopUpOrder confirmPlaceOrder(String arg0, OTP arg1, String arg2)
+			throws ServiceInventoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public TopUpOrder getTopUpOrderDetails(String arg0, String arg1)
+			throws ServiceInventoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TopUpStatus getTopUpOrderStatus(String arg0, String arg1)
+			throws ServiceInventoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TopUpQuote getTopUpQuoteDetails(String arg0, String arg1)
+			throws ServiceInventoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TopUpOrder requestPlaceOrder(String arg0, String arg1)
+			throws ServiceInventoryException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
