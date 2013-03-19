637afca68be910a55d73cb4dd87e3193d7f9dcef
Tavee Khunbida <taveek@gmail.com>
2013-03-19 14:30:52 +0700

    fix service inventory exception handler

--- service-inventory-client/src/main/java/th/co/truemoney/serviceinventory/ewallet/client/config/ServiceInventoryExceptionResponseErrorHandler.java
+++ service-inventory-client/src/main/java/th/co/truemoney/serviceinventory/ewallet/client/config/ServiceInventoryExceptionResponseErrorHandler.java
@@ -18,7 +18,9 @@ import org.springframework.web.client.UnknownHttpStatusCodeException;
 import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
 public class ServiceInventoryExceptionResponseErrorHandler extends DefaultResponseErrorHandler {
-
+	
+	public static final ServiceInventoryException SERVICE_NOT_AVAILABLE = new ServiceInventoryException("503", "Service Not Available", "TMN-PRODUCT");
+	
 	public void handleError(ClientHttpResponse response) throws IOException {
 		HttpStatus statusCode = getHttpStatusCode(response);
 
@@ -27,8 +29,10 @@ public class ServiceInventoryExceptionResponseErrorHandler extends DefaultRespon
 			try {
 				ObjectMapper objectMapper = new ObjectMapper();
 				throw objectMapper.readValue(getResponseBody(response), ServiceInventoryException.class);
-			} catch (Exception e) {
-				throw new ServiceInventoryException("503", "Service Not Available", "TMN-PRODUCT");
+			} catch (ServiceInventoryException e1) {
+				throw e1;
+			} catch (IOException e2) {
+				throw SERVICE_NOT_AVAILABLE;
 			}
 		} else {
 			throw new RestClientException("Unknown status code [" + statusCode + "]");
