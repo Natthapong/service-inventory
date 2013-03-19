d95d5f38a8a63b020de20959e0a714dab38c6a4f
schalermthai <schalermthai@wotifgroup.com>
2013-03-19 14:00:26 +0700

    added a good user profile for Local proxy

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/exception/SignonServiceException.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/exception/SignonServiceException.java
@@ -5,17 +5,17 @@ import javax.servlet.http.HttpServletResponse;
 import th.co.truemoney.serviceinventory.bean.ErrorBean;
 
 public class SignonServiceException extends BaseException {
-	
+
 	private static final long serialVersionUID = 7328535407875381185L;
-	
+
 	public SignonServiceException(String code, String description) {
 		super(code, description);
 	}
-	
+
 	public SignonServiceException(String code, String description, String namespace) {
 		super(code, description, namespace);
 	}
-	
+
 	@Override
 	public ErrorBean handleExceptions(BaseException e, HttpServletResponse response) {
 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
