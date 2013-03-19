21d0afad7d3ea67ba71526899f7e1f4550fccb52
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-19 19:06:40 +0700

    add AsynService and AsyncExecutor

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/ServiceInventoryConfig.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/ServiceInventoryConfig.java
@@ -1,15 +1,20 @@
 package th.co.truemoney.serviceinventory.config;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import th.co.truemoney.serviceinventory.ewallet.OTPService;
 import th.co.truemoney.serviceinventory.ewallet.SourceOfFundService;
 import th.co.truemoney.serviceinventory.ewallet.TmnProfileService;
 import th.co.truemoney.serviceinventory.ewallet.TopUpService;
 import th.co.truemoney.serviceinventory.ewallet.domain.AccessToken;
+import th.co.truemoney.serviceinventory.ewallet.impl.AsyncService;
 import th.co.truemoney.serviceinventory.ewallet.impl.OTPServiceImpl;
 import th.co.truemoney.serviceinventory.ewallet.impl.SourceOfFundServiceImpl;
 import th.co.truemoney.serviceinventory.ewallet.impl.TmnProfileServiceImpl;
@@ -26,6 +31,7 @@ import th.co.truemoney.serviceinventory.ewallet.repositories.impl.OTPMemoryRepos
 import th.co.truemoney.serviceinventory.ewallet.repositories.impl.OrderMemoryRepository;
 
 @Configuration
+@EnableAsync
 @ComponentScan("th.co.truemoney.serviceinventory.dao")
 public class ServiceInventoryConfig {
 
@@ -79,5 +85,21 @@ public class ServiceInventoryConfig {
     @Bean @Qualifier("otpMemoryRepository")
     public OTPRepository getOTPMemoryRepository() {
     	return new OTPMemoryRepository();
+    }
+    
+    @Bean 
+    public AsyncService getAsyncService() {
+    	return new AsyncService();
+    }
+    
+    @Bean 
+    public Executor getAsyncExecutor() {
+    	ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    	executor.setCorePoolSize(7);
+    	executor.setMaxPoolSize(56);
+    	executor.setQueueCapacity(11);
+    	executor.setThreadNamePrefix("asyncExecutor-");
+    	executor.initialize();
+    	return executor;
     }
 }
