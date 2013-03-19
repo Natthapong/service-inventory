e531ca33faff1c94ec10d239b35f95f446af75ab
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-19 16:43:43 +0700

    add FAIL status

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TopUpStatus.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TopUpStatus.java
@@ -1,5 +1,5 @@
 package th.co.truemoney.serviceinventory.ewallet.domain;
 
 public enum TopUpStatus {
-	AWAITING_CONFIRM, ORDER_VERIFIED, PROCESSING, CONFIRMED
+	AWAITING_CONFIRM, ORDER_VERIFIED, PROCESSING, CONFIRMED, FAIL
 }
