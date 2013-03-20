1d441b3eefd4285a207d7e72ffa3980943a80dca
Natthapong <sendmanz@gmail.com>
2013-03-20 13:15:09 +0700

    remove attribute because data is encapsulate in sourceOfFund

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TopUpQuote.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TopUpQuote.java
@@ -6,7 +6,6 @@ import java.math.BigDecimal;
 
 public class TopUpQuote implements Serializable {
 
-
 	private static final long serialVersionUID = 2372537225330036311L;
 
 	private String ID;
@@ -15,42 +14,6 @@ public class TopUpQuote implements Serializable {
 	private String username;
 	private BigDecimal amount;
 	private BigDecimal topUpFee;
-	private String bankCode;
-	private String bankNameEn;
-	private String bankNameTh;
-	private String bankAccountNumber;
-	
-	public String getBankCode() {
-		return bankCode;
-	}
-
-	public void setBankCode(String bankCode) {
-		this.bankCode = bankCode;
-	}
-
-	public String getBankNameEn() {
-		return bankNameEn;
-	}
-
-	public void setBankNameEn(String bankNameEn) {
-		this.bankNameEn = bankNameEn;
-	}
-
-	public String getBankNameTh() {
-		return bankNameTh;
-	}
-
-	public void setBankNameTh(String bankNameTh) {
-		this.bankNameTh = bankNameTh;
-	}
-
-	public String getBankAccountNumber() {
-		return bankAccountNumber;
-	}
-
-	public void setBankAccountNumber(String bankAccountNumber) {
-		this.bankAccountNumber = bankAccountNumber;
-	}
 
 	public String getID() {
 		return ID;
