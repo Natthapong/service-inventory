82cde904ec4dcf10e4134efe0dbf70cbc3d8c713
Natthapong <sendmanz@gmail.com>
2013-03-20 09:05:10 +0700

    fix start tomcat

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/ServiceInventoryConfig.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/ServiceInventoryConfig.java
@@ -55,6 +55,11 @@ public class ServiceInventoryConfig {
     	return new OTPServiceImpl();
     }
 	
+	@Bean
+    public AsyncService getAsyncService() {
+    	return new AsyncService();
+    }
+	
     @Bean @Qualifier("accessTokenMemoryRepository")
     public AccessTokenRepository getAccessTokenMemoryRepository() {
     	AccessTokenMemoryRepository accessTokenMemoryRepository = new AccessTokenMemoryRepository();
