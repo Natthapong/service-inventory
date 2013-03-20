923de989ce256f862451aa4d22fdd30be072ead5
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-20 13:49:57 +0700

    add throws ServiceInventoryException to getTopUpOrder()

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/ewallet/repositories/OrderRepository.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/ewallet/repositories/OrderRepository.java
@@ -2,10 +2,11 @@ package th.co.truemoney.serviceinventory.ewallet.repositories;
 
 import th.co.truemoney.serviceinventory.ewallet.domain.TopUpOrder;
 import th.co.truemoney.serviceinventory.ewallet.domain.TopUpQuote;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
 public interface OrderRepository {
 	public void saveTopUpQuote(TopUpQuote topupQuote);
 	public TopUpQuote getTopUpQuote(String orderID);
 	public void saveTopUpOrder(TopUpOrder topupOrder);
-	public TopUpOrder getTopUpOrder(String orderID);
+	public TopUpOrder getTopUpOrder(String orderID) throws ServiceInventoryException;
 }
