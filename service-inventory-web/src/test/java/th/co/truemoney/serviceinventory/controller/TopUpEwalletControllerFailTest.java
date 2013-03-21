273587d96464c07caadec42dbe2244c71180cd58
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-21 13:59:18 +0700

    add TopUpEwalletControllerFailTest

---
+++ service-inventory-web/src/test/java/th/co/truemoney/serviceinventory/controller/TopUpEwalletControllerFailTest.java
@@ -0,0 +1,71 @@
+package th.co.truemoney.serviceinventory.controller;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import th.co.truemoney.serviceinventory.config.TestRedisConfig;
+import th.co.truemoney.serviceinventory.config.TestServiceInventoryConfig;
+import th.co.truemoney.serviceinventory.config.WebConfig;
+import th.co.truemoney.serviceinventory.ewallet.TopUpService;
+import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
+import th.co.truemoney.serviceinventory.ewallet.domain.TopUpOrder;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = { WebConfig.class, TestServiceInventoryConfig.class, TestRedisConfig.class })
+public class TopUpEwalletControllerFailTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private TopUpService topupServiceMock;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		this.topupServiceMock = wac.getBean(TopUpService.class);
+	}
+
+	@After
+	public void tierDown() {
+		reset(this.topupServiceMock);
+	}
+
+	@Test
+	public void confirmPlaceOrderSuccess() throws Exception {
+		OTP otp = new OTP("112233", "885bdbcc4186d862a7ed3bae4dd3adb3b7de186a");
+		//given
+		when(topupServiceMock.confirmPlaceOrder(anyString(), any(OTP.class), anyString())).thenReturn(new TopUpOrder());
+		
+		ObjectMapper mapper = new ObjectMapper();
+		this.mockMvc.perform(post("/top-up/order/{topUpOrderID}/confirm?accessTokenID=1234567", "1")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(mapper.writeValueAsBytes(otp)))
+			.andExpect(status().isBadRequest())
+			.andDo(print());
+
+	}
+
+}
