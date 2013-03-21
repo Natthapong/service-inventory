18807e49104bcd0f32d1fa510120cfc540efee67
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-21 11:49:14 +0700

    add checkRequestPlaceOrder

--- service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/TmnTopupServiceClientRealDataTest.java
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/TmnTopupServiceClientRealDataTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import th.co.truemoney.serviceinventory.ewallet.client.config.ServiceInventoryClientConfig;
 import th.co.truemoney.serviceinventory.ewallet.domain.DirectDebit;
+import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
 import th.co.truemoney.serviceinventory.ewallet.domain.QuoteRequest;
 import th.co.truemoney.serviceinventory.ewallet.domain.TopUpOrder;
 import th.co.truemoney.serviceinventory.ewallet.domain.TopUpQuote;
@@ -43,6 +45,19 @@ public class TmnTopupServiceClientRealDataTest {
 			assertEquals("500", e.getErrorCode());
 			assertEquals("INTERNAL_SERVER_ERROR", e.getErrorDescription());
 			assertEquals("TMN-SERVICE-INVENTORY", e.getErrorNamespace());
+		}
+	}
+	
+	@Test
+	public void checkRequestPlaceOrder(){
+		try{
+			OTP otp = new OTP("112233", "663f78927872f867d883179378a12dde7ae6a71c");			
+			TopUpOrder topUpOrder = topupServiceClient.confirmPlaceOrder("1", otp, "12345");
+			assertNotNull(topUpOrder);
+			System.out.println("finished call remote:" + new Date());
+		} catch(ServiceInventoryException e){
+			System.out.println(e.getErrorCode());
+			assertEquals("1004", e.getErrorCode());
 		}
 	}
 
