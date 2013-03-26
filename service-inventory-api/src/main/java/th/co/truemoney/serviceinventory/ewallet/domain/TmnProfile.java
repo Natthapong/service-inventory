22ed83f79a42eb760ef8bdcecb31a010ac55ed79
Natthapong <sendmanz@gmail.com>
2013-03-26 11:48:12 +0700

    add attribute thaiID

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TmnProfile.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/domain/TmnProfile.java
@@ -6,13 +6,14 @@ import java.math.BigDecimal;
 public class TmnProfile implements Serializable {
 	
 	private static final long serialVersionUID = 8341135419244797163L;
-	private String fullname;
-	private String mobileno;
 	private String email;
+	private String password;
+	private String fullname;
+	private String thaiID;
+	private String mobileno;
 	private BigDecimal balance;
 	private String type;
 	private Integer status;
-	private String password;
 	
 	public TmnProfile() {
 		super();
@@ -22,40 +23,11 @@ public class TmnProfile implements Serializable {
 		this.fullname = fullname;
 		this.balance = balance;
 	}
-	
-	public String getFullname() {
-		return fullname;
-	}
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
-	}
-	public String getMobileno() {
-		return mobileno;
-	}
-	public void setMobileno(String mobileno) {
-		this.mobileno = mobileno;
-	}
-	public BigDecimal getBalance() {
-		return balance;
-	}
-	public void setBalance(BigDecimal balance) {
-		this.balance = balance;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -66,5 +38,54 @@ public class TmnProfile implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}		
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getThaiID() {
+		return thaiID;
+	}
+
+	public void setThaiID(String thaiID) {
+		this.thaiID = thaiID;
+	}
+
+	public String getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
 }
