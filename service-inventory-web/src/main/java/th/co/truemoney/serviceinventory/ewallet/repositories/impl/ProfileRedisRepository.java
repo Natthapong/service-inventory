5491cd9c8019e129566e4597eea40d0f003ae774
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-26 18:21:06 +0700

    implement ProfileRedisRepository

---
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/ewallet/repositories/impl/ProfileRedisRepository.java
@@ -0,0 +1,51 @@
+package th.co.truemoney.serviceinventory.ewallet.repositories.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import th.co.truemoney.serviceinventory.dao.RedisLoggingDao;
+import th.co.truemoney.serviceinventory.ewallet.domain.TmnProfile;
+import th.co.truemoney.serviceinventory.ewallet.repositories.ProfileRepository;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
+
+public class ProfileRedisRepository implements ProfileRepository {
+
+	private static Logger logger = LoggerFactory.getLogger(ProfileRedisRepository.class);
+
+	@Autowired
+	private RedisLoggingDao redisLoggingDao;
+	
+	@Override
+	public void saveProfile(TmnProfile tmnProfile) {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			redisLoggingDao.addData("profile:"+tmnProfile.getMobileno(), mapper.writeValueAsString(tmnProfile), 5L);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new ServiceInventoryException(ServiceInventoryException.Code.GENERAL_ERROR,
+					"Can not stored data in repository.");
+		}
+	}
+
+	@Override
+	public TmnProfile getTmnProfile(String mobileNo) {
+		try {
+			String result = redisLoggingDao.getData("profile:"+mobileNo);
+			if(result == null) {
+				throw new ServiceInventoryException(ServiceInventoryException.Code.PROFILE_NOT_FOUND,
+						"profile not found.");
+			}
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.readValue(result, TmnProfile.class);
+		} catch (ServiceInventoryException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return null;
+	}
+
+}
