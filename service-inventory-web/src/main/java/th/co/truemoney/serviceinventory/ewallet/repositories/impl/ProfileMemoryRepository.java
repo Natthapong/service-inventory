dbd1027c225176530ce3fd3ae11896bf01edb2bd
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-27 10:36:21 +0700

    create ProfileMemoryRepository

---
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/ewallet/repositories/impl/ProfileMemoryRepository.java
@@ -0,0 +1,23 @@
+package th.co.truemoney.serviceinventory.ewallet.repositories.impl;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import th.co.truemoney.serviceinventory.ewallet.domain.TmnProfile;
+import th.co.truemoney.serviceinventory.ewallet.repositories.ProfileRepository;
+
+public class ProfileMemoryRepository implements ProfileRepository {
+
+	public static HashMap<String, TmnProfile> profilesMap = new LinkedHashMap<String, TmnProfile>();
+	
+	@Override
+	public void saveProfile(TmnProfile tmnProfile) {
+		profilesMap.put(tmnProfile.getMobileno(), tmnProfile);
+	}
+
+	@Override
+	public TmnProfile getTmnProfile(String mobileNo) {
+		return profilesMap.get(mobileNo);
+	}
+
+}
