e0f5d7a22c329eeda514797c6f6476d7ba07e589
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-03-27 10:36:40 +0700

    add Bean ProfileRepository

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/MemRepositoriesConfig.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/MemRepositoriesConfig.java
@@ -7,9 +7,12 @@ import org.springframework.context.annotation.Profile;
 import th.co.truemoney.serviceinventory.ewallet.repositories.AccessTokenRepository;
 import th.co.truemoney.serviceinventory.ewallet.repositories.OTPRepository;
 import th.co.truemoney.serviceinventory.ewallet.repositories.OrderRepository;
+import th.co.truemoney.serviceinventory.ewallet.repositories.ProfileRepository;
 import th.co.truemoney.serviceinventory.ewallet.repositories.impl.AccessTokenMemoryRepository;
 import th.co.truemoney.serviceinventory.ewallet.repositories.impl.OTPMemoryRepository;
 import th.co.truemoney.serviceinventory.ewallet.repositories.impl.OrderMemoryRepository;
+import th.co.truemoney.serviceinventory.ewallet.repositories.impl.ProfileMemoryRepository;
+import th.co.truemoney.serviceinventory.ewallet.repositories.impl.ProfileRedisRepository;
 
 @Configuration
 @Profile("mem")
@@ -29,5 +32,10 @@ public class MemRepositoriesConfig {
     public OTPRepository memOTPRepository() {
     	return new OTPMemoryRepository();
     }
+    
+    @Bean
+	public ProfileRepository memoProfileRepository() {
+		return new ProfileMemoryRepository();
+	}
 
 }
