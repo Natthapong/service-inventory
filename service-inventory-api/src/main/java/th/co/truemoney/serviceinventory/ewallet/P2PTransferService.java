1c36c64f3cf976957658934c17425137e994f4e4
Natthapong <sendmanz@gmail.com>
2013-03-28 11:30:53 +0700

    add throwable

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/P2PTransferService.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/ewallet/P2PTransferService.java
@@ -5,19 +5,26 @@ import th.co.truemoney.serviceinventory.ewallet.domain.P2PDraftRequest;
 import th.co.truemoney.serviceinventory.ewallet.domain.P2PDraftTransaction;
 import th.co.truemoney.serviceinventory.ewallet.domain.P2PTransaction;
 import th.co.truemoney.serviceinventory.ewallet.domain.P2PTransactionStatus;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
 public interface P2PTransferService {
 	
-	public P2PDraftTransaction createDraftTransaction(P2PDraftRequest p2pDraftRequest, String accessTokenID);
+	public P2PDraftTransaction createDraftTransaction(P2PDraftRequest p2pDraftRequest, String accessTokenID)
+			throws ServiceInventoryException;
 
-	public P2PDraftTransaction getDraftTransactionDetail(String draftTransactionID, String accessTokenID);
+	public P2PDraftTransaction getDraftTransactionDetail(String draftTransactionID, String accessTokenID)
+			throws ServiceInventoryException;
+			
+	public P2PDraftTransaction sendOTP(String draftTransactionID, String accessTokenID)
+			throws ServiceInventoryException;
+			
+	public P2PTransaction createTransaction(String draftTransactionID, OTP otp, String accessTokenID)
+			throws ServiceInventoryException;
 	
-	public P2PDraftTransaction sendOTP(String draftTransactionID, String accessTokenID);
+	public P2PTransactionStatus getTransactionStatus(String transactionID, String accessTokenID)
+			throws ServiceInventoryException;
 	
-	public P2PTransaction createTransaction(String draftTransactionID, OTP otp, String accessTokenID);
-	
-	public P2PTransactionStatus getTransactionStatus(String transactionID, String accessTokenID);
-	
-	public P2PTransaction getTransactionDetail(String transactionID, String accessTokenID);
+	public P2PTransaction getTransactionDetail(String transactionID, String accessTokenID)
+			throws ServiceInventoryException;
 	
 }
