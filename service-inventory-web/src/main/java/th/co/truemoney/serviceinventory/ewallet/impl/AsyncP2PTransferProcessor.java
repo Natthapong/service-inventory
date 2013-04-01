8c776f9bfa69726109a765826bc34299c0365444
Natthapong <sendmanz@gmail.com>
2013-04-01 13:49:12 +0700

    add new AsynP2PTransferProcessor

---
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/ewallet/impl/AsyncP2PTransferProcessor.java
@@ -0,0 +1,14 @@
+package th.co.truemoney.serviceinventory.ewallet.impl;
+
+import th.co.truemoney.serviceinventory.ewallet.domain.P2PTransaction;
+import th.co.truemoney.serviceinventory.ewallet.proxy.message.TransferRequest;
+
+public class AsyncP2PTransferProcessor {
+
+	public void transferEwallet(P2PTransaction p2pTransaction,
+			String accessTokenID, TransferRequest transferRequest) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
