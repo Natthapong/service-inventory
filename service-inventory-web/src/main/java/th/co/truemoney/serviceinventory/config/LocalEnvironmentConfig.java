4647131bced7335e34f9d07f487fd3337cac6ec4
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-04-11 07:25:14 +0700

    remove StubEmailService Bean stub javaMailSender, freemarkerConfigurationFactory, emailEncoding, emailSender, welcomeSubject and welcomeTemplate

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/LocalEnvironmentConfig.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/LocalEnvironmentConfig.java
@@ -1,14 +1,24 @@
 package th.co.truemoney.serviceinventory.config;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.Properties;
 
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 
 import th.co.truemoney.serviceinventory.email.EmailService;
-import th.co.truemoney.serviceinventory.email.StubEmailService;
 import th.co.truemoney.serviceinventory.ewallet.exception.EwalletException;
 import th.co.truemoney.serviceinventory.ewallet.proxy.ewalletsoap.EwalletSoapProxy;
 import th.co.truemoney.serviceinventory.ewallet.proxy.message.AddMoneyRequest;
@@ -331,7 +341,92 @@ public class LocalEnvironmentConfig {
 	}
 
 	@Bean
-	public EmailService emailService(){
-		return new StubEmailService();
+	public JavaMailSender stubJavaMailSender() {
+		return new JavaMailSender() {
+
+			@Override
+			public void send(SimpleMailMessage simpleMessage)
+					throws MailException {
+				try {
+					System.out.println("stubJavaMailSender.send start");
+					Thread.sleep(3000);
+					System.out.println("stubJavaMailSender.send stop");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}								
+			}
+
+			@Override
+			public void send(SimpleMailMessage[] simpleMessages)
+					throws MailException {
+				// TODO Auto-generated method stub				
+			}
+
+			@Override
+			public MimeMessage createMimeMessage() {				
+				return new MimeMessage(Session.getInstance(new Properties()));
+			}
+
+			@Override
+			public MimeMessage createMimeMessage(InputStream contentStream)
+					throws MailException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public void send(MimeMessage mimeMessage) throws MailException {
+				try {
+					System.out.println("stubJavaMailSender.send start");
+					Thread.sleep(3000);
+					System.out.println("stubJavaMailSender.send stop");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}				
+			}
+
+			@Override
+			public void send(MimeMessage[] mimeMessages) throws MailException {							
+			}
+
+			@Override
+			public void send(MimeMessagePreparator mimeMessagePreparator)
+					throws MailException {
+				// TODO Auto-generated method stub				
+			}
+
+			@Override
+			public void send(MimeMessagePreparator[] mimeMessagePreparators)
+					throws MailException {
+				// TODO Auto-generated method stub				
+			}			
+		};
+	}
+	
+	@Bean
+	public FreeMarkerConfigurationFactory stubFreeMarkerConfigurationFactory() {
+		FreeMarkerConfigurationFactory freeMarkerConfigurationFactory = new FreeMarkerConfigurationFactory();
+		freeMarkerConfigurationFactory.setTemplateLoaderPath("email-template");
+		return freeMarkerConfigurationFactory;
+	}
+	
+	@Bean @Qualifier("emailEncoding")
+	public String getEmailEncoding() {
+		return "utf-8";
+	}
+	
+	@Bean @Qualifier("emailSender")
+	public String getEmailSender() {
+		return "emailSender";
+	}
+
+	@Bean @Qualifier("welcomeSubject")
+	public String getWelcomeSubject() {
+		return "welcomeSubject";
+	}
+
+	@Bean @Qualifier("welcomeTemplate")
+	public String getWelcomeTemplate() {
+		return "welcome-email.ftl";
 	}
 }
