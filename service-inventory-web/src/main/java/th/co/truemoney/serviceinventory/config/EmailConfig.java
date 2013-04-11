7124e37181d5883c0d24d75b391ffabbad0077d2
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-04-11 07:22:37 +0700

    add profile("dev")

--- service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/EmailConfig.java
+++ service-inventory-web/src/main/java/th/co/truemoney/serviceinventory/config/EmailConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,6 +14,7 @@ import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 
 
 @Configuration
+@Profile("dev")
 public class EmailConfig {
 	
 	@Value("${email.host}")
