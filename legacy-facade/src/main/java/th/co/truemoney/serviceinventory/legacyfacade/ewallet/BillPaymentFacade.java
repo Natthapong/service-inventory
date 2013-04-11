78ebe27bf522f0f570ad338e066264dc7cb49824
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-04-11 17:57:18 +0700

    add outline verify(BillPaymentInfo)

--- legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
+++ legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
@@ -5,6 +5,17 @@ import th.co.truemoney.serviceinventory.bill.domain.BillPaymentInfo;
 public class BillPaymentFacade {
 	
 	public BillPaymentInfo getBarcodeInformation(String barcode) {		
+		
 		return new BillPaymentInfo();
+	}
+	
+	public BillPaymentInfo verify(BillPaymentInfo billpayInfo) {
+		// parse billpayInfo to billpayRequest + functionID
+		
+		// parse obj to str xml and call billpay service
+		
+		// check billpayResponse result_code="0"
+
+		return null;
 	}
 }
