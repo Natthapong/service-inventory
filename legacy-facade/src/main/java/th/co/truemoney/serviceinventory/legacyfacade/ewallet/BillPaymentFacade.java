de75cc71ecae4070c5e0b76ff474dbbd2fa65be5
Natthapong <sendmanz@gmail.com>
2013-04-11 18:23:21 +0700

    implement get barcode information in bill payment facade

--- legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
+++ legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
@@ -1,13 +1,30 @@
 package th.co.truemoney.serviceinventory.legacyfacade.ewallet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import th.co.truemoney.serviceinventory.bill.domain.BillPaymentInfo;
+import th.co.truemoney.serviceinventory.billpay.domain.BillPayRequest;
+import th.co.truemoney.serviceinventory.billpay.domain.BillPayResponse;
+import th.co.truemoney.serviceinventory.billpay.exception.BillException;
+import th.co.truemoney.serviceinventory.billpay.exception.FailResultCodeException;
+import th.co.truemoney.serviceinventory.billpay.proxy.impl.BillPayProxy;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
+
 
 public class BillPaymentFacade {
 	
-	public BillPaymentInfo getBarcodeInformation(String barcode) {		
-		
-		return new BillPaymentInfo();
-	}
+	@Autowired
+	private BillPayProxy billPayProxy;
+	
+	private String appUser;	
+	private String appPassword;
+	private String channel;
+	private String channelDetail;
+	private String command;
+	private String functionID;
+	private String serviceNo;
+	
+	private String getBarcodeInformationURL;
 	
 	public BillPaymentInfo verify(BillPaymentInfo billpayInfo) {
 		// parse billpayInfo to billpayRequest + functionID
@@ -18,4 +35,66 @@ public class BillPaymentFacade {
 
 		return null;
 	}
+	
+	public BillPaymentInfo getBarcodeInformation(String barcode) {		
+		BillPayRequest billPayRequest = new BillPayRequest();	
+		billPayRequest.setAppUser(appUser);
+		billPayRequest.setAppPassword(appPassword);
+		billPayRequest.setChannel(channel);
+		billPayRequest.setCommand(command);
+		billPayRequest.setFunctionID(functionID);
+		billPayRequest.setServiceNo(serviceNo);
+		billPayRequest.addParameterElement("md5", "999999999");
+		billPayRequest.addParameterElement("command_action", "");
+		billPayRequest.addParameterElement("channel_detail", channelDetail);
+		billPayRequest.addParameterElement("fund_name", "barcode");
+		billPayRequest.addParameterElement("amount", "000");
+		billPayRequest.addParameterElement("currency", "THB");
+		billPayRequest.addParameterElement("barcode", barcode);
+
+		try {
+			BillPayResponse billPayResponse = billPayProxy.getBarcodeInformation(billPayRequest, getBarcodeInformationURL);
+
+			BillPaymentInfo billPaymentInfo = new BillPaymentInfo();
+			billPaymentInfo.setTarget(billPayResponse.getParameterValue().get("target"));
+
+			return billPaymentInfo;
+
+		} catch (FailResultCodeException ex) {
+			String errorNamespace = ex.getNamespace();
+			if (errorNamespace.equals("TPP")) {
+				throw new TPPSystemTransactionFailException(ex);
+			} else if (errorNamespace.equalsIgnoreCase("UMARKET")) {
+				throw new UMarketSystemTransactionFailException(ex);
+			} else {
+				throw new UnknownSystemTransactionFailException(ex);
+			}
+		}
+	}
+	
+	public static class TPPSystemTransactionFailException extends ServiceInventoryException {
+
+		private static final long serialVersionUID = 5955708376116171195L;
+
+		public TPPSystemTransactionFailException(BillException ex) {
+			super(500, ex.getCode(), "bill system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
+		}
+	}
+	
+	public static class UMarketSystemTransactionFailException extends ServiceInventoryException {
+		private static final long serialVersionUID = -162603460464737250L;
+
+		public UMarketSystemTransactionFailException(BillException ex) {
+			super(500, ex.getCode(), "umarket system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
+		}
+	}
+
+	public static class UnknownSystemTransactionFailException extends ServiceInventoryException {
+		private static final long serialVersionUID = 8166679317640543498L;
+
+		public UnknownSystemTransactionFailException(BillException ex) {
+			super(500, ex.getCode(),  "unknown system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
+		}
+	}
+
 }
