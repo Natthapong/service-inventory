d3dc607bd2ef93eca8d8ba91a1175d40831e23f2
Natthapong <sendmanz@gmail.com>
2013-04-12 01:37:50 +0700

    add implement get barcode information in bill payment facade

--- legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
+++ legacy-facade/src/main/java/th/co/truemoney/serviceinventory/legacyfacade/ewallet/BillPaymentFacade.java
@@ -1,32 +1,25 @@
 package th.co.truemoney.serviceinventory.legacyfacade.ewallet;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import th.co.truemoney.serviceinventory.bill.domain.BillPaymentInfo;
-import th.co.truemoney.serviceinventory.billpay.domain.BillPayRequest;
-import th.co.truemoney.serviceinventory.billpay.domain.BillPayResponse;
-import th.co.truemoney.serviceinventory.billpay.exception.BillException;
-import th.co.truemoney.serviceinventory.billpay.exception.FailResultCodeException;
-import th.co.truemoney.serviceinventory.billpay.proxy.impl.BillPayProxy;
+import th.co.truemoney.serviceinventory.bill.domain.BillInfo;
+import th.co.truemoney.serviceinventory.bill.domain.BillResponse;
+import th.co.truemoney.serviceinventory.bill.domain.ServiceFee;
+import th.co.truemoney.serviceinventory.bill.exception.BillException;
+import th.co.truemoney.serviceinventory.bill.exception.FailResultCodeException;
+import th.co.truemoney.serviceinventory.bill.proxy.impl.BillProxy;
 import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
 
 
 public class BillPaymentFacade {
 	
 	@Autowired
-	private BillPayProxy billPayProxy;
+	private BillProxy billPayProxy;
 	
-	private String appUser;	
-	private String appPassword;
-	private String channel;
-	private String channelDetail;
-	private String command;
-	private String functionID;
-	private String serviceNo;
-	
-	private String getBarcodeInformationURL;
-	
-	public BillPaymentInfo verify(BillPaymentInfo billpayInfo) {
+	public BillInfo verify(BillInfo billpayInfo) {
 		// parse billpayInfo to billpayRequest + functionID
 		
 		// parse obj to str xml and call billpay service
@@ -36,34 +29,67 @@ public class BillPaymentFacade {
 		return null;
 	}
 	
-	public BillPaymentInfo getBarcodeInformation(String barcode) {		
-		BillPayRequest billPayRequest = new BillPayRequest();	
-		billPayRequest.setAppUser(appUser);
-		billPayRequest.setAppPassword(appPassword);
-		billPayRequest.setChannel(channel);
-		billPayRequest.setCommand(command);
-		billPayRequest.setFunctionID(functionID);
-		billPayRequest.setServiceNo(serviceNo);
-		billPayRequest.addParameterElement("md5", "999999999");
-		billPayRequest.addParameterElement("command_action", "");
-		billPayRequest.addParameterElement("channel_detail", channelDetail);
-		billPayRequest.addParameterElement("fund_name", "barcode");
-		billPayRequest.addParameterElement("amount", "000");
-		billPayRequest.addParameterElement("currency", "THB");
-		billPayRequest.addParameterElement("barcode", barcode);
-
+	public BillInfo getBarcodeInformation(Integer channelID, String barcode) {		
 		try {
-			BillPayResponse billPayResponse = billPayProxy.getBarcodeInformation(billPayRequest, getBarcodeInformationURL);
+			BillResponse billResponse = billPayProxy.getBarcodeInformation(channelID, barcode);
 
-			BillPaymentInfo billPaymentInfo = new BillPaymentInfo();
-			billPaymentInfo.setTarget(billPayResponse.getParameterValue().get("target"));
+			BillInfo billInfo = new BillInfo();
+			billInfo.setTarget(billResponse.getParameterValue().get("target"));
+			billInfo.setLogoURL(billResponse.getParameterValue().get("logo"));
+			billInfo.setTitleTH(billResponse.getParameterValue().get("title_th"));		
+			billInfo.setTitleEN(billResponse.getParameterValue().get("title_en"));
+			
+			billInfo.setRef1TitleTH(billResponse.getParameterValue().get("ref1_title_th"));
+			billInfo.setRef1TitleEN(billResponse.getParameterValue().get("ref1_title_en"));
+			billInfo.setRef1(billResponse.getParameterValue().get("ref1"));
+			
+			billInfo.setRef2TitleTH(billResponse.getParameterValue().get("ref2_title_th"));
+			billInfo.setRef2TitleEN(billResponse.getParameterValue().get("ref2_titie_en"));
+			billInfo.setRef2(billResponse.getParameterValue().get("ref2"));
+			
+			billInfo.setPartialPayment(billResponse.getParameterValue().get("partial_payment"));
+			billInfo.setCallCenterNumber(billResponse.getParameterValue().get("call_center"));
+			
+			String amount = billResponse.getParameterValue().get("amount");
+			BigDecimal decimalAmount = new BigDecimal(amount).divide(new BigDecimal("100"));
+			billInfo.setAmount(decimalAmount.setScale(2, RoundingMode.HALF_UP));
+			
+			String minAmount = billResponse.getParameterValue().get("service_min_amount");
+			BigDecimal decimalMinAmount = new BigDecimal(minAmount).divide(new BigDecimal("100"));
+			billInfo.setMinAmount(decimalMinAmount.setScale(2, RoundingMode.HALF_UP));
+			
+			String maxAmount = billResponse.getParameterValue().get("service_max_amount");
+			BigDecimal decimalMaxAmount = new BigDecimal(maxAmount).divide(new BigDecimal("100"));
+			billInfo.setMaxAmount(decimalMaxAmount.setScale(2, RoundingMode.HALF_UP));
+					
+			ServiceFee serviceFee = new ServiceFee();
+			serviceFee.setFeeType(billResponse.getParameterValue().get("service_fee_type"));
+									
+			BigDecimal decimalServiceFee = BigDecimal.ZERO;
+			if (serviceFee.getFeeType().equals("THB")) {
+				// fee type = fix
+				String fee = billResponse.getParameterValue().get("service_fee");
+				decimalServiceFee = new BigDecimal(fee).divide(new BigDecimal("100"));
+			} else {
+				// fee type = percent
+				String fee = billResponse.getParameterValue().get("service_fee");
+				decimalServiceFee = new BigDecimal(fee);
+			}
+			serviceFee.setFee(decimalServiceFee.setScale(2, RoundingMode.HALF_UP));
+			
+			String totalServiceFee = billResponse.getParameterValue().get("total_service_fee");
+			BigDecimal decimalTotalServiceFee = new BigDecimal(totalServiceFee).divide(new BigDecimal("100"));
+			serviceFee.setTotalFee(decimalTotalServiceFee.setScale(2, RoundingMode.HALF_UP));
 
-			return billPaymentInfo;
+			billInfo.setServiceFee(serviceFee);
+			billInfo.setSourceOfFundFees(null);
+
+			return billInfo;
 
 		} catch (FailResultCodeException ex) {
 			String errorNamespace = ex.getNamespace();
-			if (errorNamespace.equals("TPP")) {
-				throw new TPPSystemTransactionFailException(ex);
+			if (errorNamespace.equals("SIENGINE")) {
+				throw new SIEngineTransactionFailException(ex);
 			} else if (errorNamespace.equalsIgnoreCase("UMARKET")) {
 				throw new UMarketSystemTransactionFailException(ex);
 			} else {
@@ -72,17 +98,16 @@ public class BillPaymentFacade {
 		}
 	}
 	
-	public static class TPPSystemTransactionFailException extends ServiceInventoryException {
-
+	public static class SIEngineTransactionFailException extends ServiceInventoryException {
 		private static final long serialVersionUID = 5955708376116171195L;
 
-		public TPPSystemTransactionFailException(BillException ex) {
+		public SIEngineTransactionFailException(BillException ex) {
 			super(500, ex.getCode(), "bill system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
 		}
 	}
 	
 	public static class UMarketSystemTransactionFailException extends ServiceInventoryException {
-		private static final long serialVersionUID = -162603460464737250L;
+		private static final long serialVersionUID = 3748885497125818864L;
 
 		public UMarketSystemTransactionFailException(BillException ex) {
 			super(500, ex.getCode(), "umarket system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
@@ -90,7 +115,7 @@ public class BillPaymentFacade {
 	}
 
 	public static class UnknownSystemTransactionFailException extends ServiceInventoryException {
-		private static final long serialVersionUID = 8166679317640543498L;
+		private static final long serialVersionUID = 5899038317339162588L;
 
 		public UnknownSystemTransactionFailException(BillException ex) {
 			super(500, ex.getCode(),  "unknown system fail with code: " + ex.getCode(), ex.getNamespace(), ex.getMessage());
