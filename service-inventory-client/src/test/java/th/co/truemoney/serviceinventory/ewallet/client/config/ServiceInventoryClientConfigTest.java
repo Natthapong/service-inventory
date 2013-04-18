10c196c4edfa1810dca6bfc5b3f942ee1169ddbf
TMN-02 <tanathip.se@gmail.com>
2013-04-18 16:19:55 +0700

    ignore ssl in vpn mode

--- service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/config/ServiceInventoryClientConfigTest.java
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/config/ServiceInventoryClientConfigTest.java
@@ -3,7 +3,6 @@ package th.co.truemoney.serviceinventory.ewallet.client.config;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
@@ -15,7 +14,7 @@ import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.BasicClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -29,10 +28,10 @@ public class ServiceInventoryClientConfigTest {
 	
 	@Bean @Primary
 	public ClientHttpRequestFactory requestFactory() throws NoSuchAlgorithmException, KeyManagementException {
-		SSLContext sslContext = SSLContext.getInstance("SSL");
 
 		// set up a TrustManager that trusts everything
-		sslContext.init(null, new TrustManager[] { new X509TrustManager() {
+		final TrustManager[] trustAllCerts = new TrustManager[] { 
+				new X509TrustManager() {
 		            public X509Certificate[] getAcceptedIssuers() {
 		                    System.out.println("getAcceptedIssuers =============");
 		                    return null;
@@ -47,15 +46,18 @@ public class ServiceInventoryClientConfigTest {
 		                            String authType) {
 		                    System.out.println("checkServerTrusted =============");
 		            }
-		} }, new SecureRandom());
+				}
+		};
 
-		SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-		Scheme httpsScheme = new Scheme("https", 443, sf);
+		final SSLContext sslContext = SSLContext.getInstance( "SSL" );
+	    sslContext.init( null, trustAllCerts, new java.security.SecureRandom() );
+	    final SSLSocketFactory sf = new SSLSocketFactory(sslContext, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+		Scheme httpsScheme = new Scheme("https", 9443, sf);
 		SchemeRegistry schemeRegistry = new SchemeRegistry();
 		schemeRegistry.register(httpsScheme);
 
 		// apache HttpClient version >4.2 should use BasicClientConnectionManager
-		ClientConnectionManager cm = new SingleClientConnManager(schemeRegistry);
+		ClientConnectionManager cm = new BasicClientConnectionManager(schemeRegistry);
 		return new HttpComponentsClientHttpRequestFactory(new DefaultHttpClient(cm));
 	}
 }
