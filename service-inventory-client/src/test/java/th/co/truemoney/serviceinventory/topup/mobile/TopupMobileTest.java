0b24a7f575f85c499bd133c058d8f45399f04ce5
TMN-02 <tanathip.se@gmail.com>
2013-04-24 11:02:11 +0700

    Add Test TopupMobileTest

---
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/topup/mobile/TopupMobileTest.java
@@ -0,0 +1,86 @@
+package th.co.truemoney.serviceinventory.topup.mobile;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import th.co.truemoney.serviceinventory.ewallet.client.TopupMobileServicesClient;
+import th.co.truemoney.serviceinventory.ewallet.client.config.EndPoints;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
+import th.co.truemoney.serviceinventory.topup.domain.TopUpMobileDraft;
+
+
+public class TopupMobileTest {
+	
+	TopupMobileServicesClient topupMobileServicesClient;
+	
+	RestTemplate restTemplate;
+	
+	EndPoints endPoints;
+	
+	HttpHeaders headers;
+	
+	@Before
+	public void setup(){
+		topupMobileServicesClient = new TopupMobileServicesClient();
+		endPoints = new EndPoints();
+		topupMobileServicesClient.setEndPoints(endPoints);
+		
+		restTemplate = mock(RestTemplate.class);
+		
+		headers = new HttpHeaders();
+		List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>();
+		acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+		headers.setAccept(acceptableMediaTypes);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		this.topupMobileServicesClient.setRestTemplate(restTemplate);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void checkVerifyURL(){
+
+		ResponseEntity<TopUpMobileDraft> responseEntity = new ResponseEntity<TopUpMobileDraft>(new TopUpMobileDraft(), HttpStatus.OK);
+		
+		when(restTemplate.exchange(eq(endPoints.getVerifyTopupMobile()), eq(HttpMethod.POST), any(HttpEntity.class)
+			, eq(TopUpMobileDraft.class) , eq("12345")) ).thenReturn(responseEntity);
+		
+		TopUpMobileDraft topUpMobileDraft = topupMobileServicesClient.verifyAndCreateTopUpMobileDraft("0839952174", new BigDecimal(0) , "12345");
+		
+		verify(restTemplate).exchange(eq(endPoints.getVerifyTopupMobile()), eq(HttpMethod.POST), any(HttpEntity.class)
+				, eq(TopUpMobileDraft.class) , eq("12345"));
+		
+	}
+	
+	@Test(expected=ServiceInventoryException.class)
+	public void checkException(){
+		when(restTemplate.exchange(eq(endPoints.getVerifyTopupMobile()), eq(HttpMethod.POST), any(HttpEntity.class)
+			, eq(TopUpMobileDraft.class) , eq("12345")) ).thenThrow(
+					new ServiceInventoryException(400,"1001","Invalid Data Exception","SI-ENGINE"));
+		
+		TopUpMobileDraft topUpMobileDraft = topupMobileServicesClient.verifyAndCreateTopUpMobileDraft("0839952174", new BigDecimal(0) , "12345");
+		
+		
+		
+	}
+}
