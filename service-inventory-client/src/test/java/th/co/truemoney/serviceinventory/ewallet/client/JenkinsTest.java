9f36129b8acbd09de85f06b4d88fc3de3bb404cd
schalermthai <schalermthai@wotifgroup.com>
2013-04-26 17:55:11 +0700

    testing failing test

---
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/JenkinsTest.java
@@ -0,0 +1,12 @@
+package th.co.truemoney.serviceinventory.ewallet.client;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JenkinsTest {
+
+	@Test
+	public void test() {
+		Assert.fail();
+	}
+}
