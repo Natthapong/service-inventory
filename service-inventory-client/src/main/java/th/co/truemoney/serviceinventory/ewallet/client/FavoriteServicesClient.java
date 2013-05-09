060e5ee4da061511488c1cc2e3c1b93b18d96292
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-05-09 14:17:59 +0700

    implement and test getFavorites()

---
+++ service-inventory-client/src/main/java/th/co/truemoney/serviceinventory/ewallet/client/FavoriteServicesClient.java
@@ -0,0 +1,57 @@
+package th.co.truemoney.serviceinventory.ewallet.client;
+
+
+import th.co.truemoney.serviceinventory.ewallet.FavoriteService;
+import th.co.truemoney.serviceinventory.ewallet.client.config.EndPoints;
+import th.co.truemoney.serviceinventory.ewallet.domain.Favorite;
+import th.co.truemoney.serviceinventory.exception.ServiceInventoryException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class FavoriteServicesClient implements FavoriteService {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@Autowired
+	private EndPoints endPoints;
+	
+	@Autowired
+	private HttpHeaders headers;
+	
+    @Override
+    public Favorite addFavorite(Favorite favorite) throws ServiceInventoryException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<Favorite> getFavorites(String serviceType, String accessTokenID) throws ServiceInventoryException {
+    	HttpEntity<String> requestEntity = new HttpEntity<String>(headers);
+		ResponseEntity<Favorite[]> responseEntity = restTemplate.exchange(
+				endPoints.getFavoritesURL(), HttpMethod.GET,
+				requestEntity, Favorite[].class, serviceType, accessTokenID);
+        return Arrays.asList(responseEntity.getBody());
+    }
+
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public void setEndPoints(EndPoints endPoints) {
+		this.endPoints = endPoints;
+	}
+
+	public void setHeaders(HttpHeaders headers) {
+		this.headers = headers;
+	}
+}
---
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/FavoriteServicesClientTest.java
@@ -0,0 +1,67 @@
+package th.co.truemoney.serviceinventory.ewallet.client;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import th.co.truemoney.serviceinventory.ewallet.client.config.EndPoints;
+import th.co.truemoney.serviceinventory.ewallet.domain.Favorite;
+
+
+public class FavoriteServicesClientTest {
+	
+	private FavoriteServicesClient client;
+	
+	private RestTemplate restTemplate;
+	
+	private EndPoints endPoints;
+	
+	private HttpHeaders headers;
+	
+	@Before
+	public void setup() {
+		restTemplate = mock(RestTemplate.class);
+		client = new FavoriteServicesClient();
+		
+		endPoints = new EndPoints();
+		client.setEndPoints(endPoints);
+		
+		headers = new HttpHeaders();
+		List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>();
+		acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+		headers.setAccept(acceptableMediaTypes);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		client.setRestTemplate(restTemplate);
+	}
+	
+	@Test
+	public void getFavorites() {
+		
+		Favorite[] favorites = new Favorite[1];
+		
+		ResponseEntity<Favorite[]> responseEntity = new ResponseEntity<Favorite[]>(favorites,HttpStatus.OK);
+		
+		when(restTemplate.exchange(eq(endPoints.getFavoritesURL()), eq(HttpMethod.GET), any(HttpEntity.class)
+				, eq(Favorite[].class) , anyString(), anyString()) ).thenReturn(responseEntity);
+		
+		List<Favorite> favoriteResults = client.getFavorites("billpay", "12345");
+		Assert.assertNotNull(favoriteResults);
+	}
+}
