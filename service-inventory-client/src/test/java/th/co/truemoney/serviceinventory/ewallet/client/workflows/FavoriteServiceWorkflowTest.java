78fbc20d14ec7a25caebdbb2c97f13d70c6da4c6
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2013-05-09 14:20:30 +0700

    implement getFavorite workflow

---
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/workflows/FavoriteServiceWorkflowTest.java
@@ -0,0 +1,51 @@
+package th.co.truemoney.serviceinventory.ewallet.client.workflows;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import th.co.truemoney.serviceinventory.ewallet.client.FavoriteServicesClient;
+import th.co.truemoney.serviceinventory.ewallet.client.TmnProfileServiceClient;
+import th.co.truemoney.serviceinventory.ewallet.client.config.LocalEnvironmentConfig;
+import th.co.truemoney.serviceinventory.ewallet.client.config.ServiceInventoryClientConfig;
+import th.co.truemoney.serviceinventory.ewallet.client.testutils.IntegrationTest;
+import th.co.truemoney.serviceinventory.ewallet.client.testutils.TestData;
+import th.co.truemoney.serviceinventory.ewallet.domain.Favorite;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ServiceInventoryClientConfig.class, LocalEnvironmentConfig.class })
+@ActiveProfiles(profiles = "local")
+@Category(IntegrationTest.class)
+public class FavoriteServiceWorkflowTest {
+	@Autowired
+	FavoriteServicesClient client;
+	
+	@Autowired
+	TmnProfileServiceClient profileService;
+	
+	@Test
+	public void getFavorites() {
+		String accessTokenID = profileService.login(
+				TestData.createSuccessUserLogin(),
+				TestData.createSuccessClientLogin());
+
+		assertNotNull(accessTokenID);
+		
+		List<Favorite> favorites = client.getFavorites("billpay", accessTokenID);
+		assertNotNull(favorites);
+		assertEquals(0, favorites.size());
+		
+		favorites = client.getFavorites(null, accessTokenID);
+		assertNotNull(favorites);
+		assertEquals(0, favorites.size());
+	}
+}
