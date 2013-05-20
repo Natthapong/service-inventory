498053a1bb1773585eb6b64bbca633493ed1894c
Natthapong <sendmanz@gmail.com>
2013-05-20 09:32:15 +0700

    add new test controller

--- service-inventory-web/src/test/java/th/co/truemoney/serviceinventory/controller/TmnProfileControllerLoginSuccessTest.java
+++ service-inventory-web/src/test/java/th/co/truemoney/serviceinventory/controller/TmnProfileControllerLoginSuccessTest.java
@@ -1,15 +1,17 @@
 package th.co.truemoney.serviceinventory.controller;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,20 +29,24 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import th.co.truemoney.serviceinventory.bean.LoginRequest;
 import th.co.truemoney.serviceinventory.config.MemRepositoriesConfig;
 import th.co.truemoney.serviceinventory.config.SmsConfig;
 import th.co.truemoney.serviceinventory.config.TestRedisConfig;
 import th.co.truemoney.serviceinventory.config.TestServiceInventoryConfig;
 import th.co.truemoney.serviceinventory.config.WebConfig;
+import th.co.truemoney.serviceinventory.ewallet.ActivityService;
 import th.co.truemoney.serviceinventory.ewallet.FavoriteService;
 import th.co.truemoney.serviceinventory.ewallet.TmnProfileService;
+import th.co.truemoney.serviceinventory.ewallet.domain.Activity;
+import th.co.truemoney.serviceinventory.ewallet.domain.ActivityDetail;
 import th.co.truemoney.serviceinventory.ewallet.domain.ClientCredential;
 import th.co.truemoney.serviceinventory.ewallet.domain.EWalletOwnerCredential;
 import th.co.truemoney.serviceinventory.ewallet.domain.Favorite;
+import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
+import th.co.truemoney.serviceinventory.ewallet.domain.TmnProfile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -57,6 +63,9 @@ public class TmnProfileControllerLoginSuccessTest {
 
 	@Autowired
 	private TmnProfileService tmnProfileServiceMock;
+	
+	@Autowired
+	private ActivityService activityServiceMock;
 	
 	@Autowired
 	private FavoriteService favoriteServiceMock;
@@ -84,8 +93,8 @@ public class TmnProfileControllerLoginSuccessTest {
 				any(ClientCredential.class)))
 				.thenReturn("8e48e03be057319f40621fe9bcd123f750f6df1d");
 		
-		EWalletOwnerCredential userLogin = new EWalletOwnerCredential("user1.test.v1@gmail.com", "e6701de94fdda4347a3d31ec5c892ccadc88b847", 40);
-		ClientCredential clientLogin = new ClientCredential("appKey", "appUser", "appPassword", "channel", "channel detail");
+		EWalletOwnerCredential userLogin = new EWalletOwnerCredential("local@tmn.com", "password", 40);
+		ClientCredential clientLogin = new ClientCredential("myAppKey", "myAppUser", "myAppPassword", "iPhone", "iPhone");
 
 		LoginRequest loginRequest = new LoginRequest(userLogin, clientLogin);
 
@@ -93,8 +102,80 @@ public class TmnProfileControllerLoginSuccessTest {
 		this.mockMvc.perform(post("/ewallet/login")
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(mapper.writeValueAsBytes(loginRequest)))
-			.andExpect(status().isOk())
-			.andExpect(content().string("8e48e03be057319f40621fe9bcd123f750f6df1d"));
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldGetProfileSuccess() throws Exception {
+		
+		//given
+		TmnProfile tmnProfile = new TmnProfile();
+		tmnProfile.setFullname("fullname");
+		tmnProfile.setMobileNumber("086xxxxxxx");
+		tmnProfile.setThaiID("1212121212121");		
+		when(this.tmnProfileServiceMock.getTruemoneyProfile(anyString())).thenReturn(tmnProfile);
+		
+		//perform
+		this.mockMvc.perform(get("/ewallet/profile/{accessTokenID}", "12345")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.fullname").value("fullname"))
+		.andExpect(jsonPath("$.thaiID").value("1212121212121"))
+		.andExpect(jsonPath("$.mobileNumber").value("086xxxxxxx"));
+		
+	}
+	
+	@Test
+	public void shouldGetBalanceSuccess() throws Exception {
+		
+		//given
+		when(this.tmnProfileServiceMock.getEwalletBalance(anyString())).thenReturn(new BigDecimal("100.00"));
+		
+		//perform
+		this.mockMvc.perform(get("/ewallet/balance/{accessTokenID}", "12345")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	public void shouldGetActivitySuccess() throws Exception {
+		Activity activity = new Activity();
+		activity.setReportID(1000l);
+		List<Activity> activities = new ArrayList<Activity>();
+		activities.add(activity);
+		
+		when(this.activityServiceMock.getActivities(anyString())).thenReturn(activities);
+		
+		this.mockMvc.perform(get("/ewallet/activities/{accessTokenID}", "12345")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())	
+		.andExpect(jsonPath("$.[0].reportID").value(1000));
+	}
+	
+	@Test
+	public void shouldGetActivityDetailSuccess() throws Exception {
+		ActivityDetail activity = new ActivityDetail();
+		activity.setTransactionID("xxx");
+		
+		when(this.activityServiceMock.getActivityDetail(anyLong(), anyString())).thenReturn(activity);
+		
+		this.mockMvc.perform(get("/ewallet/activities/{accessTokenID}/detail/{reportID}", "12345", 1000l)
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())	
+		.andExpect(jsonPath("$.transactionID").value("xxx"));
+		
+	}
+	
+	@Test
+	public void shouldLogoutSuccess() throws Exception {
+		
+		when(this.tmnProfileServiceMock.logout(anyString())).thenReturn("");
+		
+		this.mockMvc.perform(post("/ewallet/logout/{accessTokenID}", "12345")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+		
 	}
 	
 	@Test
@@ -124,5 +205,50 @@ public class TmnProfileControllerLoginSuccessTest {
 		.andExpect(status().isOk())	
 		.andExpect(jsonPath("$.[0].favoriteID").value(1000));
 	}
+	
+	@Test
+	public void shouldValidateEmailSuccess() throws Exception {
+		
+		when(this.tmnProfileServiceMock.validateEmail(anyInt(), anyString())).thenReturn("local@tmn.com");
+		
+		this.mockMvc.perform(post("/ewallet/profiles/validate-email?channelID={channelID}", "40")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes("local@tmn.com")))
+				.andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	public void shouldCreateTrueMoneyProfileSucesss() throws Exception {
+		
+		when(this.tmnProfileServiceMock.createProfile(anyInt(), any(TmnProfile.class))).thenReturn(new OTP("0866013468", "adgf"));
+		
+		TmnProfile tmnProfile = new TmnProfile();
+		tmnProfile.setFullname("fullname");
+		tmnProfile.setMobileNumber("086xxxxxxx");
+		tmnProfile.setThaiID("1212121212121");	
+		
+		this.mockMvc.perform(post("/ewallet/profiles?channelID={channelID}", "40")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(tmnProfile)))
+				.andExpect(status().isOk());
+		
+	}
 
+	@Test
+	public void shouldConfirmCreateTrueMoneyProfileSuccess() throws Exception {
+		
+		TmnProfile tmnProfile = new TmnProfile();
+		tmnProfile.setFullname("fullname");
+		tmnProfile.setMobileNumber("086xxxxxxx");
+		tmnProfile.setThaiID("1212121212121");	
+		
+		when(this.tmnProfileServiceMock.confirmCreateProfile(anyInt(), any(OTP.class))).thenReturn(tmnProfile);
+		
+		this.mockMvc.perform(post("/ewallet/profiles/verify-otp?channelID={channelID}", "40")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsBytes(new OTP("0866013468", "adgf"))))
+				.andExpect(status().isOk());
+		
+	}
 }
