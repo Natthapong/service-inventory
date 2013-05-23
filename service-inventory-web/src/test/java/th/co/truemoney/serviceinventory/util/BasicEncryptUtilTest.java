8b1db6b79f24d9ad848ea72c8ef4fbd8f40f5dce
boyone <boyone.g@gmail.com>
2013-05-23 11:16:48 +0700

    remove encryptPassword() and decryptPassword()

--- service-inventory-web/src/test/java/th/co/truemoney/serviceinventory/util/BasicEncryptUtilTest.java
+++ service-inventory-web/src/test/java/th/co/truemoney/serviceinventory/util/BasicEncryptUtilTest.java
@@ -8,18 +8,6 @@ import org.junit.Test;
 public class BasicEncryptUtilTest {
 
     @Test
-    public void encryptPassword() {
-        String encrypted = BasicEncryptUtil.encrypt("test1234");
-        Assert.assertNotNull(encrypted);
-    }
-
-    @Test
-    public void decryptPassword() {
-        String decrypted = BasicEncryptUtil.decrypt("nxOjcKl2AdXeJz6wpzXtGnxc3y1+zEQ3");
-        assertNotNull(decrypted);
-    }
-    
-    @Test
     public void encryptAndDecryptUtil() {
     	String plainTxt = "hello world";
     	String encrypted = BasicEncryptUtil.encrypt(plainTxt);
