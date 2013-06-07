b7bea12a1e747ec582c59c79727d8e2e06c8db85
Natthapong <sendmanz@gmail.com>
2013-06-07 20:18:59 +0700

    add getValueOfString inquiryOutstandingBillType

--- service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/bill/domain/InquiryOutstandingBillType.java
+++ service-inventory-api/src/main/java/th/co/truemoney/serviceinventory/bill/domain/InquiryOutstandingBillType.java
@@ -2,4 +2,7 @@ package th.co.truemoney.serviceinventory.bill.domain;
 
 public enum InquiryOutstandingBillType {
     ONLINE,OFFLINE;
+	public static InquiryOutstandingBillType valueFromString(String inquiryType) {
+		return "online".equals(inquiryType) ? ONLINE : OFFLINE;
+	}	
 }
