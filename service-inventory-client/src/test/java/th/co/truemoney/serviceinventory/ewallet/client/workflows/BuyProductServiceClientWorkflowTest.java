1f330be413e97414a26338de5d1da4398b3c2ec5
natthapong <sendmanz@gmail.com>
2013-08-25 23:46:27 +0700

    add workflow success case of buy product service

--- service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/workflows/BuyProductServiceClientWorkflowTest.java
+++ service-inventory-client/src/test/java/th/co/truemoney/serviceinventory/ewallet/client/workflows/BuyProductServiceClientWorkflowTest.java
@@ -1,5 +1,6 @@
 package th.co.truemoney.serviceinventory.ewallet.client.workflows;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import th.co.truemoney.serviceinventory.buy.domain.BuyProductDraft;
+import th.co.truemoney.serviceinventory.buy.domain.BuyProductTransaction;
 import th.co.truemoney.serviceinventory.ewallet.client.TmnBuyProductServiceClient;
 import th.co.truemoney.serviceinventory.ewallet.client.TmnProfileServiceClient;
 import th.co.truemoney.serviceinventory.ewallet.client.TransactionAuthenServiceClient;
@@ -20,6 +22,9 @@ import th.co.truemoney.serviceinventory.ewallet.client.config.LocalEnvironmentCo
 import th.co.truemoney.serviceinventory.ewallet.client.config.ServiceInventoryClientConfig;
 import th.co.truemoney.serviceinventory.ewallet.client.testutils.IntegrationTest;
 import th.co.truemoney.serviceinventory.ewallet.client.testutils.TestData;
+import th.co.truemoney.serviceinventory.ewallet.domain.OTP;
+import th.co.truemoney.serviceinventory.ewallet.domain.Transaction;
+import th.co.truemoney.serviceinventory.transfer.domain.P2PTransferDraft;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { ServiceInventoryClientConfig.class, LocalEnvironmentConfig.class })
@@ -43,11 +48,61 @@ public class BuyProductServiceClientWorkflowTest {
 		String accessTokenID = profileServiceClient.login(TestData.createSuccessUserLogin(), TestData.createSuccessClientLogin());
 		assertNotNull(accessTokenID);
 
-		// create transfer draft
+		// create buy product draft
 		BuyProductDraft buyProductDraft = tmnBuyProductServiceClient.createAndVerifyBuyProductDraft("epin_c", "0866011234", new BigDecimal("20.00"), accessTokenID);
 		assertNotNull(buyProductDraft);
 		assertNotNull(buyProductDraft.getID());
+		
+		// get buy product draft
+		buyProductDraft = tmnBuyProductServiceClient.getBuyProductDraftDetails(buyProductDraft.getID(), accessTokenID);
+		assertNotNull(buyProductDraft);
+		assertNotNull(buyProductDraft.getID());
+		assertEquals("0866011234", buyProductDraft.getRecipientMobileNumber());
+		assertEquals(BuyProductDraft.Status.CREATED, buyProductDraft.getStatus());
+		
+		// send otp and waiting confirm
+		OTP otp = authenClient.requestOTP(buyProductDraft.getID(), accessTokenID);
+		assertNotNull(otp);
+		assertNotNull(otp.getReferenceCode());
+		
+		// get buy product draft and check draft status
+		buyProductDraft = tmnBuyProductServiceClient.getBuyProductDraftDetails(buyProductDraft.getID(), accessTokenID);
+		assertEquals(P2PTransferDraft.Status.OTP_SENT, buyProductDraft.getStatus());
 
+		// confirm otp
+		otp.setOtpString("111111");
+		BuyProductDraft.Status draftStatus = authenClient.verifyOTP(buyProductDraft.getID(), otp, accessTokenID);
+		assertNotNull(draftStatus);
+		assertEquals(BuyProductDraft.Status.OTP_CONFIRMED, draftStatus);
+
+		// get buy product draft and check draft status
+		buyProductDraft = tmnBuyProductServiceClient.getBuyProductDraftDetails(buyProductDraft.getID(), accessTokenID);
+		assertEquals(P2PTransferDraft.Status.OTP_CONFIRMED, buyProductDraft.getStatus());
+
+		Transaction.Status transactionStatus = tmnBuyProductServiceClient.performBuyProduct(buyProductDraft.getID(), accessTokenID);
+		assertEquals(Transaction.Status.VERIFIED, transactionStatus);
+
+		// get order status
+		Thread.sleep(100);
+		transactionStatus = tmnBuyProductServiceClient.getBuyProductStatus(buyProductDraft.getID(), accessTokenID);
+		assertNotNull(transactionStatus);
+
+		// retry while processing
+		while (transactionStatus == Transaction.Status.PROCESSING) {
+			transactionStatus = tmnBuyProductServiceClient.getBuyProductStatus(buyProductDraft.getID(), accessTokenID);
+			Thread.sleep(1000);
+		}
+
+		// retry until success
+		assertEquals(Transaction.Status.SUCCESS, transactionStatus);
+
+		BuyProductTransaction buyProductTransaction = tmnBuyProductServiceClient.getBuyProductResult(buyProductDraft.getID(), accessTokenID);
+
+		assertNotNull(buyProductTransaction);
+		assertNotNull(buyProductTransaction.getDraftTransaction());
+		assertNotNull(buyProductTransaction.getConfirmationInfo());
+		assertEquals(Transaction.Status.SUCCESS, buyProductTransaction.getStatus());
+		
 	}
 
 }
